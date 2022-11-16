95c0b6d2e0bf120f742a1a24784fc6f1371ec19b
Achaz <jtugume123@gmail.com>
2022-11-16 19:07:17 +0300

    a few changes

--- providers.tf
+++ providers.tf
@@ -10,4 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
+  shared_credentials_file = "C:/Users/jtugu/.aws/credentials"
+  profile                 = "default"
 }
